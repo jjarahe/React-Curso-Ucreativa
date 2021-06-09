e41c123330173da8f0048b9a796736d5125f16c2
Juan Jara <jjarahe@gmail.com>
2021-06-08 18:54:06 -0600

    Proyecto Avance 1

---
+++ PythonCode/Proyecto/profile.py
@@ -0,0 +1,14 @@
+users = [
+    {
+        'username': 'admin',
+        'password': 'admin',
+        'full_name': 'Alejandro Sanchez',
+        'role': 'admin'
+    },
+    {
+        'username': 'guest',
+        'password': 'guest',
+        'full_name': 'Juan Luis Jara Herrera',
+        'role': 'guest'
+    }
+]
