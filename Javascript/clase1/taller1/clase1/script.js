1ec5ba7bcd8c238b68cb9041dc67e92d67eaadc4
Juan Luis Jara Herrera <juan.luis.jara.herrera@intel.com>
2021-09-23 20:55:16 -0600

    clase 3

--- Javascript/clase1/taller1/clase1/script.js
+++ Javascript/clase1/taller1/clase1/script.js
@@ -9,3 +9,25 @@ boton.onclick = () => {
     
 }
 console.log(document.querySelectorAll("li")[0].textContent)
+
+console.log(document.querySelector('#division1').getAttribute('id'))
+console.log(document.querySelector('#division1').setAttribute('class', 'js'))
+console.log(document.querySelector('#division1').getAttributeNames())
+console.log(document.querySelector('h1').textContent)
+document.querySelector('h1').textContent = "Test Cambiando el texto"
+console.log(document.querySelector('h1').textContent)
+
+document.querySelector("img").getAttribute("src")
+document.querySelectorAll('li')[1].textContent = 'Nuevo Texto'
+
+document.querySelectorAll("input")[0].value = prompt()
+
+const p = document.createElement("p")
+p.textContent = 'Test Test Test'
+document.querySelector("#division2").insertBefore(p, document.querySelector("form"))
+
+const listaLi = document.querySelectorAll("input[type='text']")
+
+for(let x = 0; x < listaLi.length(); x++){
+    listaLi[x].value = prompt()
+}
