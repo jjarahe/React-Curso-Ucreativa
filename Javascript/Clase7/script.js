66ff4da9029f173114e2fce9df9f1682af22fc37
Juan Luis Jara Herrera <juan.luis.jara.herrera@intel.com>
2021-10-07 21:07:12 -0600

    Clase 7
    sadas

--- Javascript/Clase7/script.js
+++ Javascript/Clase7/script.js
@@ -1,4 +1,48 @@
-function procesarFormulario() {
+function test(){
+    console.log('aqui estoy')
+    
+}
 
-    return false;
+function procesarFormulario() {
+const formData = {}
+
+document.querySelectorAll('div.name-item input').forEach( element => {
+    formData[element.placeholder.toLowerCase()] = element.value
+})
+
+formData.genero = document.querySelector('input[name:"gender"]:checked').value
+
+formData.peso = document.querySelector('input[name=peso]').value
+formData.altura = document.querySelector('input[name=altura]').value
+
+formData.direccion = document.querySelector('input[name=direccion]').value
+
+document.querySelectorAll('div.city-item input').forEach( element => {
+    formData[element.placeholder.toLowerCase()] = element.value
+})
+
+formData.provincia = document.querySelector('div.city-item select').value
+
+formData.email =  document.querySelector('input[type=email]').value
+
+const telefono = document.querySelector('input[name="telefono"]').value
+if(telefono.match(/\b\d{8}\b/)){
+    formData.telefono = document.querySelector('input[name="telefono"]').value
+} else {
+    document.querySelector('input[name="telefono"]').parentElement.children[0].classList.add('error')
+    return false
+}
+
+formData.personalTrainer = document.querySelector('input[name:"personal-trainer"]:checked').value
+
+formData.experiencia = document.querySelector('input[name:experiencia]:checked').value
+
+formData.membresia = document.querySelector('input[name:membership]:checked').value
+
+const display = document.querySelector('#display')
+display.textContent = JSON.stringify(formData, null, 4)
+display.setAttribute('class','visible')
+
+console.log(JSON.stringify(formData, null, 4))
+
 }
