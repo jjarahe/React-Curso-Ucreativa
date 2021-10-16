4d515c0ad4f4c9394253250f92c1217f4e00f90d
Juan Jara <jjarahe@gmail.com>
2021-10-15 21:48:28 -0600

    proyecto

---
+++ Javascript/Proyecto/script.js
@@ -0,0 +1,9 @@
+import * as api from 'harry_api.js'
+const boton = document.querySelector('button[type="button"]')
+
+boton.onclick =  () => {
+    console.log('testxs') 
+}
+
+
+//http://hp-api.herokuapp.com/api/characters
