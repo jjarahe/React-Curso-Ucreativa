32fd16b514d34ce703bf295f04d6bc2aed0f4f9d
Juan Jara <jjarahe@gmail.com>
2021-10-16 10:19:15 -0600

    proyecto

--- Javascript/Proyecto/harry_api.js
+++ Javascript/Proyecto/harry_api.js
@@ -5,6 +5,14 @@ let listOfCharacters = []
 async function fecthCharacter() {
     const response =  await fetch('http://hp-api.herokuapp.com/api/characters')
     const characters = await response.json()
-    listOfCharacters = characters.filter(elemento => elemento.name && elemento.image)
+    listOfCharacters = characters.filter(elemento => elemento.name && elemento.image && elemento.patronus)
     console.log('API cargada', listOfCharacters)              
+}
+
+
+//Helper
+function getRandomInt(min,max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
 }
--- Javascript/Proyecto/script.js
+++ Javascript/Proyecto/script.js
@@ -1,58 +1,84 @@
 fecthCharacter()
 
 const boton = document.querySelector('#respuesta')
+const typeOfQuestions = ['actor','name','house','patronus']
+const questions = ['Cual es actor que interpreta este personaje?','Como se llama el personaje?','Cual es la casa a la que pertenece?','Cual es su patronus?'] 
+
 
 boton.onclick =  () => {
-    console.log('testxs') 
-  
+    createQuestion()
+    
+    
 }
 
-const container = document.createElement("div")
-container.classList.add("container","d-flex","justify-content-center","mt-5")
 
-const card = document.createElement("div")
-card.classList.add("card")
-card.setAttribute("style","width: 30rem;")
+function createQuestion() {
 
-const img = document.createElement("img")
-img.src = "http://hp-api.herokuapp.com/images/ron.jpg"
-img.classList.add("card-img-top","p-2")
-img.alt = "Harry Potter imagen"
-img.setAttribute("style","height: 30rem;")
-
-const card_body = document.createElement("div")
-card_body.classList.add("card-body")
-    const titulo_pregunta = document.createElement("h5")
-    titulo_pregunta.classList.add("card-title")
-    titulo_pregunta.innerText = "Pregunta"
+    const character = listOfCharacters[getRandomInt(0,listOfCharacters.length)]
     
-    const p = document.createElement("p")
-    p.classList.add("card-text")
-    p.innerText = "Como se llama este personaje?"
-card_body.appendChild(titulo_pregunta)
-card_body.appendChild(p)
-
-const ul = document.createElement("ul")
-ul.classList.add("list-group","list-group-flush")
-    const li = document.createElement("li")
-    li.classList.add("list-group-item")
-        const input = document.createElement("input")
-        input.type = "radio"
-        input.name = "pregunta1"
-        input.id = "pregunta1A"
+    const randomQuestion = getRandomInt(0,typeOfQuestions.length)
     
-        const label = document.createElement("label")
-        label.for = "pregunta1A"
-        label.innerText = "Harry Potter"
-    li.appendChild(input)
-    li.appendChild(label)
-ul.appendChild(li)
+    const container = document.createElement("div")
+        container.classList.add("container","d-flex","justify-content-center","mt-5", "mb-5")
 
+    const card = document.createElement("div")
+        card.classList.add("card")
+        card.setAttribute("style","width: 30rem;")
 
-card.appendChild(img)
-card.appendChild(card_body)
-card.appendChild(ul)
-container.appendChild(card)
+    const img = document.createElement("img")
+        img.src = character.image
+        img.classList.add("card-img-top","p-2")
+        img.alt = "Harry Potter imagen"
+        img.setAttribute("style","height: 30rem;")
 
-const body = document.querySelector("body")
-body.appendChild(container)
+    const card_body = document.createElement("div")
+        card_body.classList.add("card-body")
+            const titulo_pregunta = document.createElement("h5")
+            titulo_pregunta.classList.add("card-title")
+            titulo_pregunta.textContent = "Pregunta"
+        
+            const p = document.createElement("p")
+            p.classList.add("card-text")
+            p.textContent = questions[randomQuestion]
+    card_body.appendChild(titulo_pregunta)
+    card_body.appendChild(p)
+
+    const ul = document.createElement("ul")
+        ul.classList.add("list-group","list-group-flush")
+        
+        for (let i = 0; i < 3; i++) {
+            
+            const li = document.createElement("li")
+            li.classList.add("list-group-item")
+                const input = document.createElement("input")
+                input.type = "radio"
+                input.name = "respuesta"
+                input.id = `respuesta${i}`
+            
+                const label = document.createElement("label")
+                label.for = `respuesta${i}`
+                const fakeCharacter = listOfCharacters[getRandomInt(0,listOfCharacters.length)]//que fake no sea igual a character para no tener 2 correctas
+                label.textContent = fakeCharacter[typeOfQuestions[randomQuestion]]
+                
+            li.appendChild(input)
+            li.appendChild(label)
+            
+            ul.appendChild(li)
+        }
+            
+    const div_boton = document.createElement("div")
+        div_boton.classList.add("card-body","d-flex","justify-content-center")
+            const button = document.createElement("button")
+            button.type = "button"
+            button.classList.add("btn","btn-primary")
+            button.id = "respuestaDinamica"
+            button.textContent = "Responder"
+            div_boton.appendChild(button)
+    card.appendChild(img)
+    card.appendChild(card_body)
+    card.appendChild(ul)
+    card.appendChild(div_boton)
+    container.appendChild(card)
+    const body = document.querySelector("body")
+    body.appendChild(container)
+}
