21f04eb4a44a60677f5903ea18f087c1b75926bf
Juan Luis Jara Herrera <juan.luis.jara.herrera@intel.com>
2021-10-19 00:00:32 -0600

    Proyecto

--- Javascript/Proyecto/harry_api.js
+++ Javascript/Proyecto/harry_api.js
@@ -5,7 +5,7 @@ let listOfCharacters = []
 async function fecthCharacter() {
     const response =  await fetch('http://hp-api.herokuapp.com/api/characters')
     const characters = await response.json()
-    listOfCharacters = characters.filter(elemento => elemento.name && elemento.image && elemento.patronus)
+    listOfCharacters = characters.filter(elemento => elemento.name && elemento.image && elemento.patronus &&  elemento.house)
     console.log('API cargada', listOfCharacters)              
 }
 
--- Javascript/Proyecto/script.js
+++ Javascript/Proyecto/script.js
@@ -3,18 +3,37 @@ fecthCharacter()
 const boton = document.querySelector('#respuesta')
 const typeOfQuestions = ['actor','name','house','patronus']
 const questions = ['Cual es actor que interpreta este personaje?','Como se llama el personaje?','Cual es la casa a la que pertenece?','Cual es su patronus?'] 
+const correctAnswers = []
+const anwersLetters = ["A","B","C","D"]
+let fakeCharacters = []
+let puntuacion = 0
+const nombreJugador = "Juan Jara"
 
-
-boton.onclick =  () => {
-    createQuestion()
-    
-    
+function loadFakeCharacters() {
+    fakeCharacters = []
+    for (let i = 0; i < listOfCharacters.length; i++) {
+        fakeCharacters.push(listOfCharacters[getRandomInt(0,listOfCharacters.length)])   
+    }
 }
 
+boton.onclick =  () => {
+    for (let index = 1; index < 4; index++) {
+        createQuestion(index)  
+        
+    }
+}
 
-function createQuestion() {
-
+function createQuestion(numOfQuestion) {
+    
     const character = listOfCharacters[getRandomInt(0,listOfCharacters.length)]
+    correctAnswers.push(character)
+    loadFakeCharacters()
+    let filteredCharacters = [...new Set(fakeCharacters)]
+    filteredCharacters.splice(3,filteredCharacters.length, character)
+    //filteredCharacters.push(character)
+    console.log(filteredCharacters)
+    
+    let answers = filteredCharacters.map((value)=> ({ value, sort: Math.random()})).sort((a, b) => a.sort - b.sort).map(({value}) => value )
     
     const randomQuestion = getRandomInt(0,typeOfQuestions.length)
     
@@ -46,38 +65,37 @@ function createQuestion() {
     const ul = document.createElement("ul")
         ul.classList.add("list-group","list-group-flush")
         
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 4; i++) {
             
             const li = document.createElement("li")
             li.classList.add("list-group-item")
                 const input = document.createElement("input")
                 input.type = "radio"
                 input.name = "respuesta"
-                input.id = `respuesta${i}`
-            
+                input.id = `respuesta-${anwersLetters[i]}-${numOfQuestion}`
+                input.value = `${answers[i][typeOfQuestions[randomQuestion]]}`
                 const label = document.createElement("label")
-                label.for = `respuesta${i}`
-                const fakeCharacter = listOfCharacters[getRandomInt(0,listOfCharacters.length)]//que fake no sea igual a character para no tener 2 correctas
-                label.textContent = fakeCharacter[typeOfQuestions[randomQuestion]]
+                label.setAttribute('for',`respuesta-${anwersLetters[i]}-${numOfQuestion}`)
+                label.textContent = `${answers[i][typeOfQuestions[randomQuestion]]}`    
                 
             li.appendChild(input)
             li.appendChild(label)
-            
             ul.appendChild(li)
         }
-            
-    const div_boton = document.createElement("div")
+    /*const div_boton = document.createElement("div")
         div_boton.classList.add("card-body","d-flex","justify-content-center")
-            const button = document.createElement("button")
-            button.type = "button"
-            button.classList.add("btn","btn-primary")
-            button.id = "respuestaDinamica"
-            button.textContent = "Responder"
-            div_boton.appendChild(button)
+            const buttonD = document.createElement("button")
+            buttonD.type = "button"
+            buttonD.classList.add("btn","btn-primary")
+            buttonD.textContent = "Responder"
+            buttonD.setAttribute("onclick", alert("clicked"))
+            div_boton.appendChild(buttonD)*/
+            
+        
     card.appendChild(img)
     card.appendChild(card_body)
     card.appendChild(ul)
-    card.appendChild(div_boton)
+    //card.appendChild(div_boton)
     container.appendChild(card)
     const body = document.querySelector("body")
     body.appendChild(container)
